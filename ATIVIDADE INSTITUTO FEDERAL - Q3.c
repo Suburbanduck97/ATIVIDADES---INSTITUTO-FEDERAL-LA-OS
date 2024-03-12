8867034de352ef7708eafcdf321fef88418a9186
Ericsson dos Santos <ericssonsantos24@gmail.com>
2024-03-11 22:35:48 -0300

    Add files via upload
    3.	Faça	 um	 programa	em	 C	 (com	a	estrutura	 do...while)	 que	leia	 20	 valores	
    inteiros	e:	
    –	Encontre	e	mostre	o	maior	valor;	
    –	Encontre	e	mostre	o	menor	valor;	
    –	Calcule	e	mostre	a	média	dos	números	lidos;

---
+++ ATIVIDADE INSTITUTO FEDERAL - Q3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+	int numero = 0, i, soma = 0, contador, maiorNumero = 0, menorNumero = 1;
+	float media = 0;
+	
+	setlocale(LC_ALL, "portuguese");
+	
+	do{
+		printf("INSIRA O %dª NÚMERO: ", contador);
+		scanf("%d",&numero);
+		
+		contador = contador + 1;
+		soma = soma + numero;
+		media = soma / 20;
+		
+		if(numero > maiorNumero){
+		maiorNumero = numero;
+	}else if(numero < menorNumero){
+		menorNumero = numero;
+	}
+
+		
+	}while(contador <= 20);
+	
+	printf("MAIOR VALOR %d \n", maiorNumero);
+	printf("MENOR VALOR %d",menorNumero);
+	
+	
+}
